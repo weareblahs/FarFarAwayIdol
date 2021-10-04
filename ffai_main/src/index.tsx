926ecaf401cacb5b6276d27a1e079128f476fadc
thisistyx-AUTO <tanyuxuan2005@gmail.com>
2021-10-04 18:49:44 +0800

    Ready to reset

--- ffai_main/src/index.tsx
+++ ffai_main/src/index.tsx
@@ -31,16 +31,16 @@ function introvideo() {
 function votingscreen() {
   return (
     <section data-during="vote/">
-            <div id="shrekandfiona"><img src="assets/shrekandfiona.png"></div>
-            <div id="pussinboots"><img src="assets/pussinboots.png"></div>
-            <div id="hook"><img src="assets/hook.png"></div>
-            <div id="gingy"><img src="assets/gingy.png"></div>
-            <div id="mice"><img src="assets/mice.png"></div>
-            <div id="charming"><img src="assets/charming.png"></div>
-            <div id="bbwtlp"><img src="assets/bbwtlp.png"></div>
-            <div id="doris"><img src="assets/doris.png"></div>
-            <div id="pinocchio"><img src="assets/pinocchio.png"></div>
-            <div id="donkey"><img src="assets/donkey.png"></div>
+            <div id="shrekandfiona"><img src="./assets/shrekandfiona.png"></div>
+            <div id="pussinboots"><img src="./assets/pussinboots.png"></div>
+            <div id="hook"><img src="./assets/hook.png"></div>
+            <div id="gingy"><img src="./assets/gingy.png"></div>
+            <div id="mice"><img src="./assets/mice.png"></div>
+            <div id="charming"><img src="./assets/charming.png"></div>
+            <div id="bbwtlp"><img src="./assets/bbwtlp.png"></div>
+            <div id="doris"><img src="./assets/doris.png"></div>
+            <div id="pinocchio"><img src="./assets/pinocchio.png"></div>
+            <div id="donkey"><img src="./assets/donkey.png"></div>
     </section>
   );
 }
